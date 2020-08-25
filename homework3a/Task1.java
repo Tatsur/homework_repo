f8d264fdbd80f11ff9928c67ed67f781171a3405
Ivan_Polovnikov <i.a.polovnikov@gmail.com>
2020-08-25 18:01:29 +0700

    homework for lesson 3

---
+++ homework3a/Task1.java
@@ -0,0 +1,68 @@
+package ru.geekbrains.java_one.homework.homework3a;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        guessTheNumber(scanner, random);
+        scanner.close();
+    }
+
+    private static void guessTheNumber(Scanner scanner, Random random) {
+        int value;
+        int triesCount = 3;
+        int randomValue = random.nextInt(10);
+        System.out.println("Guess the number from 0 to 10: ");
+
+        while (triesCount > 0) {
+            triesCount--;
+
+            if (scanner.hasNextInt())
+                value = scanner.nextInt();
+            else {
+                System.out.println("Incorrect input!\nPlease enter an integer value: ");
+                scanner.nextLine();
+                continue;
+            }
+
+            if (value == randomValue) {
+                System.out.println("Congratulations! You guessed the number!");
+                if (tryAgain(scanner, random))
+                    break;
+            } else if (value < randomValue) {
+                System.out.println("The number is greater than you guessed");
+                nextTryPrintOut(triesCount);
+            } else {
+                System.out.println("The number is less than you guessed");
+                nextTryPrintOut(triesCount);
+            }
+
+            if (triesCount <= 0) {
+                System.out.println("You've run out of attempts! You lost!");
+                if (tryAgain(scanner, random))
+                    break;
+            }
+        }
+    }
+
+    private static void nextTryPrintOut(int triesCount) {
+        System.out.println("Attempts left: " + triesCount);
+        if (triesCount > 0) {
+            System.out.println("Guess the number: ");
+        }
+    }
+
+    private static boolean tryAgain(Scanner scanner, Random random) {
+        int value;
+        System.out.println("Do you want to play again? 1 - yes / 0 - no");
+        value = scanner.nextInt();
+        if (value == 1) {
+            guessTheNumber(scanner, random);
+        } else return true;
+        return false;
+    }
+}
---
+++ homework3a/Task2.java
@@ -0,0 +1,51 @@
+package ru.geekbrains.java_one.homework.homework3a;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task2 {
+    public static Scanner scanner = new Scanner(System.in);
+    public static Random random = new Random();
+    public static String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado",
+            "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak",
+            "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear",
+            "pepper", "pineapple", "pumpkin", "potato"};
+    public static String targetWord = words[random.nextInt(words.length)];
+    public static int targetWordLength = targetWord.length();
+    public static char maskChar = '#';
+    public static String maskNull = "";
+
+    public static void main(String[] args) {
+
+        StringBuilder mask = new StringBuilder();
+
+        for (int i = 0; i < 15; i++) {
+            maskNull += maskChar;
+        }
+        mask.replace(0,mask.length(),maskNull);
+        System.out.println(maskNull);
+        System.out.println("Guess the word: ");
+        guessTheWord(mask);
+    }
+
+    private static void guessTheWord(StringBuilder mask) {
+        String guessWord = scanner.next();
+        guessWord = guessWord.toLowerCase();
+        if (targetWord.equals(guessWord)) {
+            System.out.println("Congratulation! You guessed the word!");
+        } else {
+            int limits = Math.min(targetWordLength, guessWord.length());
+            for (int i = 0; i < limits; i++) {
+                if (targetWord.charAt(i) == guessWord.charAt(i)) {
+                    mask.deleteCharAt(i);
+                    mask.insert(i, guessWord.charAt(i));
+                }
+            }
+            System.out.println(mask);
+            System.out.println("Oops! You are mistaken :(");
+            System.out.println("Guess the word again: ");
+            mask.replace(0,mask.length(),maskNull);
+            guessTheWord(mask);
+        }
+    }
+}
